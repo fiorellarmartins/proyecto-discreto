54d859a5e62a55cdb9258a7b100af0db5c54ca2f
lazlo <gonzalo0902@hotmail.com>
2022-06-02 12:55:58 -0500

    estandarizado

--- lecturadebases.py
+++ lecturadebases.py
@@ -1,30 +1,19 @@
 import pandas as pd
 import numpy as np
 
-def calcular_media(df):
-    vector_media=['']
-    for i in range(1,df.shape[1]-1):
-        media=0.0
-        for j in range(df.shape[0]):
-            media+=float(df.iat[j,i])
-        media=media/df.shape[0]
-        vector_media.append(round(media,2))
-    return(vector_media)
-def binoomo(df,media):
-    binomo=np.zeros(shape=(len(df),len(df.columns)-2))
+def binoomo(df):
+    binomo=np.zeros(shape=(len(df),len(df.columns)-1))
     for i in range(len(binomo)):
-        for j in range(len(binomo[i])):
+        for j in range(len(binomo[i])+1):
             if j>0:
-                if df.iat[i,j]>media[j]:
-                    binomo[i][j]=1
+                if float(df.iat[i,j])>0.5:
+                    binomo[i][j-1]=1
     return binomo
             
 
 df = pd.read_csv("test.csv")
 print(df)
-medias=calcular_media(df)
-print(medias)
-mat=binoomo(df,medias)
+mat=binoomo(df)
 print(mat)
 
 
