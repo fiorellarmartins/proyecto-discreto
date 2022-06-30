e1cec5a3e32d7817e3422461891257a8681d0c61
lazlo <gonzalo0902@hotmail.com>
2022-06-30 18:26:40 -0500

    fiorela con doble l

--- maincode.py
+++ maincode.py
@@ -54,6 +54,7 @@ class base:
 dat=os.path.dirname(os.path.realpath('maincode.py'))+'\df\lasbases'
 ar=open(dat,'rb')
 L=pickle.load(ar)
+ar.close()
 
 ###IMPRESION para ver los contenidos en el dat
 '''for i in range(len(L)):
@@ -132,8 +133,62 @@ KOREA=base(dfkr,'KOREA',Corea)
 BRA=base(dfbra,'BRASIL',Brasil)
 JAP=base(dfjap,'JAPAN',Japon)'''
 
-#SUBIR CAMBIOS
+### SUBIR CAMBIOS
 '''L=[USA,KOREA,BRA,JAP]
 ar=open(dat,'wb')
 pickle.dump(L,ar)
+ar.close()'''
+
+### Crear csv comparacion resultados/benchmark
+'''prueba=[]
+for i in range(143):
+    A=[0,0,0,0,0,0,0,0,0]
+    prueba.append(A)
+
+prueba[0][0]='Fecha'
+prueba[0][1]='Us code'
+prueba[0][2]='US bench'
+prueba[0][3]='Japon code'
+prueba[0][4]='Japon bench'
+prueba[0][5]='Korea code'
+prueba[0][6]='Korea Bench'
+prueba[0][7]='Brasil code'
+prueba[0][8]='Brasil bench'
+
+bench=[]
+ass=os.path.dirname(os.path.realpath(__file__))+'\df\sbench.csv'
+benchread = open(ass,"r")
+for linea in benchread:
+    bench.append(linea.split(","))
+benchread.close()
+
+for i in range(1,len(prueba)):
+    for j in range(len(prueba[0])):
+        if j==1:
+            if i in L[0].filaspositivas:
+                prueba[i][j]=1
+        elif j==2:
+            prueba[i][j]=bench[i][5]
+        elif j==3:
+            if i in L[3].filaspositivas:
+                prueba[i][j]=1
+        elif j==4:
+            prueba[i][j]=bench[i][6]
+        elif j==5:
+            if i in L[1].filaspositivas:
+                prueba[i][j]=1
+        elif j==6:
+            prueba[i][j]=bench[i][7]
+        elif j==7:
+            if i in L[2].filaspositivas:
+                prueba[i][j]=1
+        elif j==8:
+            prueba[i][j]=bench[i][8][0]
+        elif j==0:
+            prueba[i][j]=bench[i][0]
+
+ass=os.path.dirname(os.path.realpath(__file__))+'\df\cresultados.csv'
+ar=open(ass,"w")
+for i in range(143):
+    ar.write('{0},{1},{2},{3},{4},{5},{6},{7},{8},\n'.format(prueba[i][0],prueba[i][1],prueba[i][2],prueba[i][3],prueba[i][4],prueba[i][5],prueba[i][6],prueba[i][7],prueba[i][8]))
 ar.close()'''
