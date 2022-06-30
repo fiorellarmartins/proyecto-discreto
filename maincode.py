c929ff83b448aab504b247df49eb8176f8959512
lazlo <gonzalo0902@hotmail.com>
2022-06-30 18:11:16 -0500

    Carolina

--- maincode.py
+++ maincode.py
@@ -7,24 +7,27 @@ class base:
     def __init__(self,base,pais,comparison):
         self.nombre_pais=pais
         self.base=base
-        self.estandarizado=self.binoomo(base)
-        self.filaspositivas=self.check(comparison)
+        self.estandarizado=self.binoomo(base,base)
+        self.IPE=comparison
+        self.filaspositivas=self.check(self.estandarizado)
+        self.input=None
+        self.input_results=None
 
-    def binoomo(self,df):
-        binomo=np.zeros(shape=(len(df),len(df.columns)))
+    def binoomo(self,dfcom,dfref):
+        binomo=np.zeros(shape=(len(dfcom),len(dfcom.columns)))
         for j in range(len(binomo[0])):
-            avg=df[j].mean()
+            avg=dfref[j].mean()
             for i in range(len(binomo)):
-                numero=df.iat[i,j]
+                numero=dfcom.iat[i,j]
                 if numero>avg:
                     binomo[i][j]=1
         return binomo
 
     def check(self,base):
-        matrx_xcom=self.estandarizado
+        matrx_xcom=base
         control,valoresconfirmados=[],[]
-        for i in range(len(base)):
-            c=list(base[i])
+        for i in range(len(self.IPE)):
+            c=list(self.IPE[i])
             control.append(c)
         for proposicion in range(len(matrx_xcom)):
             for implicante in range(len(control)):
@@ -39,43 +42,80 @@ class base:
                     break
         return valoresconfirmados
 
+    def operacion_usuario(self,input):
+        self.input=input
+        estandarizao=self.binoomo(input,self.base)
+        self.input_results=self.check(estandarizao)
+
     def __str__(self):
         return 'Soy '+self.nombre_pais
 
 ###Las bases
-dat=os.path.dirname(os.path.realpath(__file__))+'\df\lasbases'
+dat=os.path.dirname(os.path.realpath('maincode.py'))+'\df\lasbases'
 ar=open(dat,'rb')
 L=pickle.load(ar)
-ar.close()
 
-for i in range(len(L)):
+###IMPRESION para ver los contenidos en el dat
+'''for i in range(len(L)):
     print(L[i])
-    print(L[i].filaspositivas)
+    print(L[i].filaspositivas)'''
 
+####MENU para presentacion final
+print('  Escoge el pais a revisar  '.center(50,'#'),'\n\n','1. USA'.center(43,' '),'\n','2. Corea'.center(46,' '),'\n','3. Brasil'.center(48,' '),'\n','4. Japon\n'.center(48,' '))
 
-###GUARDAR LA ESTANDARIZACION EN CSV
+while True:
+    x=input('>>>Escoge tu opcion: ')
+    print('\n','#'*50,'\n')
+    if x.lower()=='q':
+        break
+    elif int(x)==1:
+        try:
+            dat=os.path.dirname(os.path.realpath('maincode.py'))+r'\###CSV ACA###\USA_input.csv'
+            df = pd.read_csv(dat,header=None)
+            L[int(x)-1].operacion_usuario(df)
+            A=L[int(x)-1].input
+            B=L[int(x)-1].input_results
+            print('Input: \n',A,'\n\nResultados: \n',B)
+            print('\n','#'*50,'\n')
+            break
+        except: print('ERROR en el CSV, (',dat,')\n')
+    elif int(x)==2:
+        try:
+            dat=os.path.dirname(os.path.realpath('maincode.py'))+r'\###CSV ACA###\COREA_input.csv'
+            df = pd.read_csv(dat,header=None)
+            L[int(x)-1].operacion_usuario(df)
+            A=L[int(x)-1].input
+            B=L[int(x)-1].input_results
+            print(L[int(x)-1],'\n',L[int(x)-1].filaspositivas)
+            print('Input: \n',A,'\n\nResultados: \n',B)
+            print('\n','#'*50,'\n')
+            break
+        except: print('ERROR en el CSV, (',dat,')\n')
+    elif int(x)==3:
+        try:
+            dat=os.path.dirname(os.path.realpath('maincode.py'))+r'\###CSV ACA###\BRASIL_input.csv'
+            df = pd.read_csv(dat,header=None)
+            L[int(x)-1].operacion_usuario(df)
+            A=L[int(x)-1].input
+            B=L[int(x)-1].input_results
+            print('Input: \n',A,'\n\nResultados: \n',B)
+            print('\n','#'*50,'\n')
+            break
+        except: print('ERROR en el CSV, (',dat,')\n')
+    elif int(x)==4:
+        try:
+            dat=os.path.dirname(os.path.realpath('maincode.py'))+r'\###CSV ACA###\JAPON_input.csv'
+            df = pd.read_csv(dat,header=None)
+            L[int(x)-1].operacion_usuario(df)
+            A=L[int(x)-1].input
+            B=L[int(x)-1].input_results
+            print('Input: \n',A,'\n\nResultados: \n',B)
+            print('\n','#'*50,'\n')
+            break
+        except: print('ERROR en el CSV, (',dat,')\n')
+    else:print('Selecciona una opcion del menu\n','\n','#'*50,'\n')
 
-'''csvkorea=os.path.dirname(os.path.realpath(__file__))+'\df\korea_estandarizado.csv'
-csvbrasil=os.path.dirname(os.path.realpath(__file__))+'\df\sbrasil_estandarizado.csv'
-csvjapon=os.path.dirname(os.path.realpath(__file__))+'\df\japon_estandarizado.csv'
-csvus=os.path.dirname(os.path.realpath(__file__))+'\df\estdosunidos_estandarizado.csv'
-for objeto in range(len(L)):
-    matrix=L[objeto].estandarizado.astype(int)
-    if L[objeto].pais=="USA":
-        ar=open(csvus,'w')
-    elif L[objeto].pais=="BRASIL":
-        ar=open(csvbrasil,'w')
-    elif L[objeto].pais=="KOREA":
-        ar=open(csvkorea,'w')
-    elif L[objeto].pais=="JAPAN":
-        ar=open(csvjapon,'w')
-    else: print('wtf')
-    for i in range(len(matrix)):
-        ar.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}\n".format(matrix[i][0],matrix[i][1],matrix[i][ 2],matrix[i][ 3],matrix[i][ 4],matrix[i][ 5],matrix[i][ 6],matrix[i][ 7],matrix[i][ 8],matrix[i][ 9],matrix[i][ 10],matrix[i][ 11],matrix[i][ 12]))
-        #ar.write("{0}{1}{2}{3}{4}{5}{6}{7}{8}{9}{10}{11}{12}\n".format(matrix[i][0],matrix[i][1],matrix[i][ 2],matrix[i][ 3],matrix[i][ 4],matrix[i][ 5],matrix[i][ 6],matrix[i][ 7],matrix[i][ 8],matrix[i][ 9],matrix[i][ 10],matrix[i][ 11],matrix[i][ 12]))
-    ar.close()'''
-
-### La he cagado y cambie atributos, el paquete:
+### Reiniciar atributos:
 '''dat=os.path.dirname(os.path.realpath(__file__))+'\df\lasbases'
 csvkorea=os.path.dirname(os.path.realpath(__file__))+'\df\korea.csv'
 csvbrasil=os.path.dirname(os.path.realpath(__file__))+'\df\sbrasil.csv'
@@ -90,20 +130,10 @@ dfjap = pd.read_csv(csvjapon,header=None)
 USA=base(dfus,"USA",USA)
 KOREA=base(dfkr,'KOREA',Corea)
 BRA=base(dfbra,'BRASIL',Brasil)
-JAP=base(dfjap,'JAPAN',Japon)
+JAP=base(dfjap,'JAPAN',Japon)'''
 
 #SUBIR CAMBIOS
-L=[USA,KOREA,BRA,JAP]
+'''L=[USA,KOREA,BRA,JAP]
 ar=open(dat,'wb')
 pickle.dump(L,ar)
 ar.close()'''
-
-
-
-'''ar=open(csvbrasil,'w')
-for i in range(len(lista)):
-    ar.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12}\n".format(lista.iloc[i, 0],lista.iloc[i, 1],lista.iloc[i, 2],lista.iloc[i, 3],lista.iloc[i, 4],lista.iloc[i, 5],lista.iloc[i, 6],lista.iloc[i, 7],lista.iloc[i, 8],lista.iloc[i, 9],lista.iloc[i, 10],lista.iloc[i, 11],lista.iloc[i, 12]))
-ar.close()
-'''
-
-
